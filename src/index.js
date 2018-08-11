bf939e3bbe8c1bdd1090dab9a98245bd3bc88f7e
libjared <libjared@users.noreply.github.com>
2018-08-10 20:05:55 -0500

    Circle demo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
-// let stage = new createjs.stage;
+import createjs from 'createjs';
 
-// let circle = new createjs.Shape();
-// circle.graphics.beginFill("red").drawCircle(0,0,40);
+const stage = new createjs.Stage("demoCanvas");
 
-alert("Fuck you");
+const gameState = {
+    testX: 50,
+    testY: 50
+};
+
+function startup() {
+    stage.addEventListener("stagemousedown", mouseDown);
+    createjs.Ticker.timingMode = createjs.Ticker.RAF;
+    createjs.Ticker.addEventListener("tick", tick);
+
+    let circle = new createjs.Shape();
+    circle.graphics.beginFill("red").drawCircle(0, 0, 40);
+    circle.x = gameState.testX;
+    circle.y = gameState.testY;
+    stage.addChild(circle);
+    stage.update();
+}
+
+function tick(event) {
+    const deltaS = event.delta / 1000;
+    // update everything game related here.
+	stage.update(event);
+}
+
+function mouseDown(event) {
+    console.log(event);
+}
+
+
+startup();
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        alias: {
+            createjs: 'createjs/builds/1.0.0/createjs.js'
+        }
+    },
+    module: {
+        rules: [{
+            test: /node_modules[/\\]createjs/,
+            loaders: [
+                'imports-loader?this=>window',
+                'exports-loader?window.createjs'
+            ]
+        }]
+    }
+};
