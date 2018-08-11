1b2db3887dc9db9b53685e38469f0311fb276c8a
libjared <libjared@users.noreply.github.com>
2018-08-10 19:38:54 -0500

    Add webpack

--- index.js
+++
@@ -1,4 +0,0 @@
-let stage = new createjs.stage;
-
-let circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0,0,40);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// let stage = new createjs.stage;
+
+// let circle = new createjs.Shape();
+// circle.graphics.beginFill("red").drawCircle(0,0,40);
+
+alert("Fuck you");
