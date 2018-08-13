d9d33b41fd4cb0795f257591c49bcc07bbb50d49
Scott Weidenkopf <sweidenkopf@gmail.com>
2018-08-13 18:15:18 -0400

    fixed instructions

--- dist/main.js
+++ dist/main.js
@@ -425,11 +425,11 @@ function instructionsState(game) {
 
     let instructionsTextStyle = { font: '10pt Press Start 2P', fill: 'white', align: 'left', wordWrap: true, wordWrapWidth: game.width-60};
     let instructionsText = 'Gain points for every prisoner who dies!!!\n\n'
-                         + '1. Move by right clicking.\n\n'
+                         + '1. Move by left clicking.\n\n'
                          + '2. Prisoners will queue up by the door. Click on them to attend to them.\n\n'
-                         + '3. While attending to a prisoner, you can excort them to a cell or to the chopping block.\n\n'
-                         + '4. Cells can hold two prisoners, and they interact with eachother. Some prisoners kill eachother (good),'
-                         + 'but if two prisoners of the same type share a cell for too long, they can escape!\n\n'
+                         + '3. While attending to a prisoner, you can escort them to a cell or to the chopping block.\n\n'
+                         + '4. Cells can hold two prisoners, and they interact with each other. Some prisoners kill each other (good),'
+                         + 'but if two prisoners of the same type share a cell for too long, they can escape (bad)!\n\n'
                          + '5. Each prisoner who escapes takes one of your lives with them!\n\n'
                          + '6. The chopping block requires the axe to be sharpened after each use (its availability can be seen at the top).';
 
